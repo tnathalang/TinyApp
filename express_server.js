f2e2278ba1c2b436f05c96c96e87b3cb95e1ef5f
vagrant <nathalang_t@hotmail.ca>
2019-01-16 17:49:43 +0000

    Added functionality of generating random alphanumerical string when adding a new URL, currently the delete function is broken and the path for deleted urls is not properly defined

--- express_server.js
+++ express_server.js
@@ -1,14 +1,43 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const uuidv1 = require("uuid/v1");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  const id = uuidv1().substring(0, 6);
+  // const alphanumeric =
+  //   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456759";
+  // let result = [];
+  // for (let i = 0; i < 6; i++) {
+  //   result.push(alphanumeric[Math.floor(Math.random() * alphanumeric.length)]);
+  // }
+
+  // return result.join("");
+
+  return id;
+}
+function deleteURL() {
+  delete urlDatabase.body.shortURL;
+}
+
+app.post("/urls", (req, res) => {
+  const randomId = generateRandomString();
+
+  urlDatabase[randomId] = req.body.longURL;
+  res.redirect("/");
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -16,7 +45,31 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  let templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  const removedURL = deleteURL();
+
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
